97af35b4a56e45033540f552f03e8871bf7d2b5a
scottandrewlepera <scott@scottandrew.com>
2024-09-20 23:05:05 -0700

    updates

---
+++ js/bs-overrides-2024.js
@@ -0,0 +1,8 @@
+/*
+JS used to override page styles. Each must be wrapped in a <script> tag to work.
+To apply, open the page in the SQS editor, then use Settings > Advanced > Inject custom code.
+*/
+
+// Latest (linktree)
+document.addEventListener("DOMContentLoaded", (event) => {document.body.classList.add("bs-linktree");});
+
